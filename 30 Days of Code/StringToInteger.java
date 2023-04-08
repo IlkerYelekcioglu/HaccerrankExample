d1472b7a051055e735e17e51272bf0fbc3284432
İlker Yelekçioğlu <53173162+IlkerYelekcioglu@users.noreply.github.com>
2023-04-09 02:21:26 +0300

    Add files via upload

---
+++ 30 Days of Code/StringToInteger.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class StringToInteger {
+        public static void main(String[] args)  {
+            Scanner in = new Scanner(System.in);
+            String string = in.next();
+            in.close();
+            try {
+                Integer integer = Integer.parseInt(string);
+                System.out.println(integer);
+            } catch (NumberFormatException numberFormatException) {
+                System.out.println("Bad String");
+            }
+        }
+}
+
+
