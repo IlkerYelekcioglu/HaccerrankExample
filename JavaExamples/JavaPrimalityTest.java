a4348d47dc6ef0f617f29ec153f7695da34e413f
İlker Yelekçioğlu <53173162+IlkerYelekcioglu@users.noreply.github.com>
2023-04-09 02:22:41 +0300

    Add files via upload

---
+++ JavaExamples/JavaPrimalityTest.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class JavaPrimalityTest {
+    public static void main(String[] args)
+    {
+        try (Scanner scanner = new Scanner(System.in))
+        {
+            System.out.println(scanner.nextBigInteger().isProbablePrime(100) ? "prime" : "not prime");
+        }
+    }
+}
